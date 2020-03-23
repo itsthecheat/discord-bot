c370587bbb028e8e319ad7813677900c92d08b37
Leslie Behum <llaurenb@gmail.com>
2020-03-23 15:02:46 -0400

    first commit

---
+++ bot.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
+client.login("NjkwNTYyMzQ4NjgyODM4MDY3.XnTPGQ.Mq4nNkQ19otZ94TPdmXkZV--NOo")
+
+
+
+// const eris = require('eris');
+//
+// // Create a Client instance with our bot token.
+// const bot = new eris.Client('NjkwNTYyMzQ4NjgyODM4MDY3.XnTPGQ.Mq4nNkQ19otZ94TPdmXkZV--NOo');
+//
+// // When the bot is connected and ready, log to console.
+// bot.on('ready', () => {
+//    console.log('Connected and ready.');
+// });
+//
+// // Every time a message is sent anywhere the bot is present,
+// // this event will fire and we will check if the bot was mentioned.
+// // If it was, the bot will attempt to respond with "Present".
+// bot.on('messageCreate', async (msg) => {
+//    const botWasMentioned = msg.mentions.find(
+//        mentionedUser => mentionedUser.id === bot.user.id,
+//    );
+//
+//    if (botWasMentioned) {
+//        try {
+//            await msg.channel.createMessage('Present');
+//        } catch (err) {
+//            // There are various reasons why sending a message may fail.
+//            // The API might time out or choke and return a 5xx status,
+//            // or the bot may not have permission to send the
+//            // message (403 status).
+//            console.warn('Failed to respond to mention.');
+//            console.warn(err);
+//        }
+//    }
+// });
+//
+// bot.on('error', err => {
+//    console.warn(err);
+// });
+//
+// bot.connect();
