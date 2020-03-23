cede387919621814b373c7657facd5aa46f00094
Leslie Behum <llaurenb@gmail.com>
2020-03-23 15:40:12 -0400

    test

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
